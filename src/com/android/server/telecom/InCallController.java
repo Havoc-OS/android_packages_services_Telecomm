Flamingo-OS/packages_services_Telecomm

Telecomm: fix vibrate feedback not being provided with call connect for incoming
* Removed current outgoing call connect vibrate effect, used click effect instead in InCallController

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

src/com/android/server/telecom/InCallController.java
@@ -1307,11 +1307,10 @@ public class InCallController extends CallsManagerListenerBase implements
     @Override
     public void onCallStateChanged(Call call, int oldState, int newState) {
         maybeTrackMicrophoneUse(isMuted());
-        final boolean vibrate = Settings.System.getIntForUser(
-            mContext.getContentResolver(),
-            Settings.System.INCALL_FEEDBACK_VIBRATE,
-            0, UserHandle.USER_CURRENT) == 1;
-        if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrate) {
+        final boolean vibrate = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.INCALL_FEEDBACK_VIBRATE, 0, UserHandle.USER_CURRENT) == 1;
+        if ((oldState == CallState.ANSWERED || oldState == CallState.DIALING) &&
+                newState == CallState.ACTIVE && vibrate) {
             vibrate(CALL_CONNECT_EFFECT);
         } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED && vibrate) {
             vibrate(CALL_DISCONNECT_EFFECT);

Havoc-OS/android_packages_services_Telecomm

Change-Id: I363ed151f15f032971662a05c608a7b1604d8752
Signed-off-by: Pranav <npv12@iitbbs.ac.in>
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/android/server/telecom/InCallController.java
@@ -1314,7 +1314,8 @@ public class InCallController extends CallsManagerListenerBase implements
         boolean vibrateOnDisconnect = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.INCALL_FEEDBACK_VIBRATE, 0, UserHandle.USER_CURRENT) == 1;
 
-        if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrateOnConnect) {
+        if ((oldState == CallState.ANSWERED || oldState == CallState.DIALING) &&
+                newState == CallState.ACTIVE && vibrateOnConnect) {
             performHapticFeedback(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
         } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
                 && vibrateOnDisconnect) {